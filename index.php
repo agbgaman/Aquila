331b760b3dcbe23d6dfc0e75b3e75391acacd6bd
agbgaman <aksen1111ns@gmail.com>
2024-01-31 12:34:21 +0530

    new files

--- index.php
+++ index.php
@@ -3,19 +3,19 @@
  * Main template file 
  *  
  *
- * @pacakage Aquila
+ * @package Aquila
  */
 
+ get_header();
  ?>
 
- <!DOCTYPE html>
- <html lang="en">
- <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wordpress Theme</title>
- </head>
- <body>
-    <h1>hello aman</h1>
- </body>
- </html>
+
+<div class="content">
+    Content
+</div>
+
+<?php
+
+get_footer();
+
+    
